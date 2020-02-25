eeae4bf0caa6ff69f55e2126afafac8bc3888f37
Jelle Eysbach <J.T.Eysbach@student.tudelft.nl>
2020-02-25 13:29:57 +0100

    Added EJS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
+// Initialise the environment
 dotenv.config();
 const app = express();
-const port = process.env.PORT; // default port to listen
+const port = process.env.PORT || 8080; // get port from env, otherwise take default
+
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
