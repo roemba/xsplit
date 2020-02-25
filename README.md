6841cb80ff4e6fe7f0368ebaa1e5045e1a97d527
Jelle Eysbach <J.T.Eysbach@student.tudelft.nl>
2020-02-25 13:00:28 +0100

    Added dotenv dependency

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 billsplitting
+
+TypeScript Express application in node.js
+
+Used packages:
+DotEnv is used to import configuration into the system. Mainly used for the database settings.
+When building project, input correct parameters in .env.example and rename it to .env
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.send( "Hello world! " + process.env.TEST);
 } );
 
 // start the Express server
