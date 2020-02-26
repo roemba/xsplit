6829522b2e37dd389a13b4d6216b2104756a28bf
astamagg <asta.magg94@gmail.com>
2020-02-26 15:39:54 +0100

    Connection fix between express and controller

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -1,4 +1,4 @@
-import {Controller, Param, Body, Get, Post, Put, Delete, Render, CurrentUser, Authorized} from "routing-controllers";
+import {Controller, Param, Body, Get, Post, Put, Delete, Render, Res, Req, Authorized, CurrentUser} from "routing-controllers";
 import { Container } from "typedi";
 import { UserService } from "../services/UserService";
 import winston, { Logger } from "winston";
@@ -6,7 +6,7 @@ import { RippleLibService } from "../services/RippleLibService";
 import { User } from "../models/User";
 import { GetServerInfoResponse } from "ripple-lib/dist/npm/common/serverinfo";
 
-@Controller("")
+@Controller("/api")
 export class AppController {
    log: Logger;
    constructor() {
@@ -21,11 +21,6 @@ export class AppController {
       return Container.get(RippleLibService).getServerInfo();
    }
 
-   @Get("/users")
-   getAllUsers(): Promise<User[]> {
-      return Container.get(UserService).find();
-   }
-
    @Get("/web/:page")
    @Render("index.ejs")
    getEJSView(@Param("page") page: string): unknown {
@@ -33,33 +28,10 @@ export class AppController {
       return {page};
    }
 
-   @Get("/users/:id")
-   getOne(@Param("id") id: number): string {
-      return "This action returns user #" + id;
-   }
-
    @Authorized()
    @Get("/me")
    getMe(@CurrentUser({ required: true }) me: User): User {
       return me;
-   }
-
-   @Post("/users")
-   post(@Body() thing: User): string {
-      this.log.info(thing);
-      return "Posting thing...";
-   }
-
-   @Put("/users/:id")
-   put(@Param("id") id: number, @Body() thing: User): string {
-      this.log.info("Updating " + id + " " + thing);
-      return "Updating a thing...";
-   }
-
-   @Delete("/users/:id")
-   remove(@Param("id") id: number): string {
-      this.log.info("Deleting " + id);
-      return "Deleting thing...";
    }
 
 }
