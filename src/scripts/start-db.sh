469eb6a967f1d194a93a429924a880ed82f65fe8
Claudiu Ion <c.ion@student.tudelft.nl>
2020-03-03 07:54:52 +0100

    added script for db creation

---
+++ src/scripts/start-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+SERVER="postgres";
+PW="admin";
+DB="test";
+
+echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
+(docker kill $SERVER || :) && \
+  (docker rm $SERVER || :) && \
+  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
+  -e PGPASSWORD=$PW \
+  -p 5432:5432 \
+  -d postgres
+
+# wait for pg to start
+echo "sleep wait for pg-server [$SERVER] to start";
+SLEEP="/bin/sleep"
+$SLEEP 4;
+
+# create the db 
+echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
+echo "\l" | docker exec -i $SERVER psql -U postgres
