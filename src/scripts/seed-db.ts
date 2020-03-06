624da233ae87bd6969fcf4b0b8c6a5604998cf2c
cldme <ioncld97@gmail.com>
2020-03-06 13:33:15 +0100

    handling users already in db

--- src/scripts/seed-db.ts
+++ src/scripts/seed-db.ts
@@ -15,13 +15,39 @@ const logger = winston.createLogger({
 useContainer(Container);
 dotenv.config();
 
-async function addUser(username: string, publickey: string, email: string, fullName: string): Promise<User> {
+async function addUser(username: string, publickey: string, email: string, fullName: string): Promise<void> {
     const user = new User();
     user.username = username;
     user.publickey = publickey;
     user.email = email;
     user.fullName = fullName;
-    return Container.get(UserService).create(user);
+    Container.get(UserService).create(user).then(() => {
+        logger.info("Added new user to db!");
+    }).catch(() => {
+        logger.info("User already exists in db!");
+    });
+}
+
+async function seedDB(): Promise<void> {
+    // tests users
+    const users = {
+        "alice": {
+            "username": "alice",
+            "publickey": "02C90CDEDE88AFD56FF51A41DDF8B12EB0380D3F4D21D2BB6CD15E64FEB25358F6",
+            "email": "alice@xsplit.com",
+            "name": "Alice"
+        },
+        "bob": {
+            "username": "bob",
+            "publickey": "02247DCA3727D848340F1520968A2191D3AC8F1299AFC7291969E6845AC7CFB579",
+            "email": "bob@xsplit.com",
+            "name": "Bob"
+        }
+    }
+
+    // add test users to db
+    addUser(users.alice.username, users.alice.publickey, users.alice.email, users.alice.name);
+    addUser(users.bob.username, users.bob.publickey, users.bob.email, users.bob.name);
 }
 
 async function setupTypeORM(): Promise<void> {
@@ -45,27 +71,9 @@ async function setupTypeORM(): Promise<void> {
 setupTypeORM().then(() => {
     logger.info("Connected to db!");
 
-    // tests users
-    const users = {
-        "alice": {
-            "username": "alice",
-            "publickey": "02C90CDEDE88AFD56FF51A41DDF8B12EB0380D3F4D21D2BB6CD15E64FEB25358F6",
-            "email": "alice@xsplit.com",
-            "name": "Alice"
-        },
-        "bob": {
-            "username": "bob",
-            "publickey": "02247DCA3727D848340F1520968A2191D3AC8F1299AFC7291969E6845AC7CFB579",
-            "email": "bob@xsplit.com",
-            "name": "Bob"
-        }
-    }
-
-    // add test users to db
-    addUser(users.alice.username, users.alice.publickey, users.alice.email, users.alice.name);
-    addUser(users.bob.username, users.bob.publickey, users.bob.email, users.bob.name);
-
-    logger.info("Done seeding the db!");
+    seedDB().then(() => {
+        logger.info("Done seeding the db!");
+    });
 
 }).catch((e) => {
     logger.error("Db connection failed with error: " + e);
