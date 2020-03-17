25be8ec0d2cea4013a804fa2b0ece6a3a78986ec
Joost <j.w.bambacht@student.tudelft.nl>
2020-03-17 18:49:05 +0100

    Added Lists pages
    Removed friend pages since we don't want that anymore. List overview successes to list detail page. All dummy info right now.

--- tools/bundleFrontendModules.ts
+++ tools/bundleFrontendModules.ts
@@ -4,5 +4,5 @@ import * as shell from "shelljs";
 shell.exec("browserify dist/events/LoginScript.js > dist/events/LoginScriptBundled.js");
 shell.exec("browserify dist/events/RegisterScript.js > dist/events/RegisterScriptBundled.js");
 shell.exec("browserify dist/events/AccountScript.js > dist/events/AccountScriptBundled.js");
-shell.exec("browserify dist/events/FriendScript.js > dist/events/FriendScriptBundled.js");
 shell.exec("browserify dist/events/RequestScript.js > dist/events/RequestScriptBundled.js");
+shell.exec("browserify dist/events/ListsScript.js > dist/events/ListsScriptBundled.js");
