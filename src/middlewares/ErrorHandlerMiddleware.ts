52b7f6f7c71469a3c99c1c2591a5fe4ec22f2675
Roemer Hendrikx <roemba@pm.me>
2020-03-25 17:41:00 +0100

    Make ValidationErrors visible in BadRequestErrors

--- src/middlewares/ErrorHandlerMiddleware.ts
+++ src/middlewares/ErrorHandlerMiddleware.ts
@@ -1,19 +1,27 @@
-import { Container } from "typedi";
+import {Container} from "typedi";
 import * as express from 'express';
-import { ExpressErrorMiddlewareInterface, HttpError, Middleware } from 'routing-controllers';
-import { LoggerService } from "../services/LoggerService";
+import {ExpressErrorMiddlewareInterface, Middleware} from 'routing-controllers';
+import {LoggerService} from "../services/LoggerService";
+
+class ExpressError {
+    httpCode: number;
+    message: string;
+    name: string;
+    errors: object[];
+}
 
 @Middleware({ type: 'after' })
 export class ErrorHandlerMiddleware implements ExpressErrorMiddlewareInterface {
 
     log = Container.get(LoggerService);
 
-    public error(error: HttpError, req: express.Request, res: express.Response): void {
+    public error(error: ExpressError, req: express.Request, res: express.Response): void {
         res.status(error.httpCode || 500);
         res.statusMessage = error.message;
         res.json({
             name: error.name,
-            message: error.message
+            message: error.message,
+            errors: error.errors
         });
         
         this.log.error(error.message);
