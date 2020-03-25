6a26bd32c2b5fb653d8e3ced9c7f1210bccd6d66
Roemer Hendrikx <roemba@pm.me>
2020-03-25 18:18:17 +0100

    Add missing Full name registration field and validation to User update handler

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -17,23 +17,43 @@ import {LoggerService} from "../services/LoggerService";
 import 'babel-polyfill';
 import * as brandedQRCode from 'branded-qr-code';
 import path from "path";
-import {IsEmail, IsNotEmpty, IsString, MaxLength} from "class-validator";
+import {IsBoolean, IsEmail, IsNotEmpty, IsString, MaxLength} from "class-validator";
 
 class CreateUserRequest {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(1000)
-  username: string;
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(1000)
+    username: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(4000)
-  publickey: string;
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(1000)
+    fullName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(4000)
+    publickey: string;
+}
+
+class UpdateUserRequest {
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(1000)
+    fullName: string;
+
+    @IsBoolean()
+    notifications: boolean
 }
 
 @JsonController("/api/users")
@@ -49,8 +69,13 @@ export class UserController {
 
     @Put("")
     @Authorized()
-    putMe(@CurrentUser() user: User): string {
-      return "Updating the user " + user.username;
+    async putMe(@CurrentUser() user: User, @Body() body: UpdateUserRequest): Promise<string> {
+      user.email = body.email;
+      user.fullName = body.fullName;
+      user.notifications = body.notifications;
+
+      await Container.get(UserService).update(user);
+      return "User updated";
     }
 
     @Delete("")
@@ -71,6 +96,7 @@ export class UserController {
         user.username = body.username;
         user.email = body.email;
         user.publickey = body.publickey;
+        user.fullName = body.fullName;
 
         const newUser = await Container.get(UserService).create(user);
         if (newUser.username !== undefined) {
@@ -79,13 +105,6 @@ export class UserController {
 
 
         throw new BadRequestError("Public key and/or username is already in use");
-    }
-
-    @Put("")
-    @Authorized()
-    put(@CurrentUser() user: User, @Body() body: User): string {
-      Container.get(UserService).update(user.username, body);
-      return "Updating the user";
     }
 
     @Get("/qr/:username")
